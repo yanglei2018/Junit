4d24351679c3e3312825b308f978fe3ddc34e88c
yanglei2020 <760236741@qq.com>
2020-07-31 09:28:10 +0800

    第一次提交

---
+++ Junit/src/FirstJunit/calculator.java
@@ -0,0 +1,25 @@
+package FirstJunit;
+/*
+计算器类
+ */
+public class calculator {
+    /**
+     *加法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a , int b ){
+        return a + b;
+    }
+
+    /**
+     *减法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int sub(int a , int b ){
+        return a + b;
+    }
+}
